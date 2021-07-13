77bd2a2a6ccc9ce797ac19b90a7df5d4c98d9000
Daniel Borisov <daniel.borisov@VIMIS-BORISD-LM.local>
2021-07-13 17:09:36 +0300

    new design

---
+++ src/theme/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@material-ui/core";
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main:'#005879',
+            
+        },
+        secondary:{
+            main:'#0088CC'
+        }
+    }
+})
+

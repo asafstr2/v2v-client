9b59459a82f1a027d470a03b30beeed0d54c15b1
Daniel Borisov <daniel.borisov@VIMIS-BORISD-LM.local>
2021-07-13 17:02:49 +0300

    new design

--- src/component/content/Content.jsx
+++ src/component/content/Content.jsx
@@ -6,7 +6,22 @@ import {addTodo} from "../../redux/actions";
 import contentStyles from "./content-styles";
 import SimpleModal from "../modal/simple-modal";
 import MenteeInput from "../mentee/mentee-input";
+import { Typography ,makeStyles} from "@material-ui/core";
+import { ReactComponent as ButtonArrow } from "../../assets/ButtonArrow.svg";
+const useStyles = makeStyles(theme=>({
+    title:{
+        color:theme.palette.secondary.main,
+        fontSize:48,
+        fontWeight:800,
+        textAlign:'left'
+    },
+    addButton:{
+        padding:12,
+        color:theme.palette.secondary.main,
+        alignSelf:'flex-end'
 
+    }
+}))
 function Content() {
     const classes = contentStyles();
     const dispatch = useDispatch();
@@ -22,18 +37,20 @@ function Content() {
     const addBubble = (text) => {
         dispatch(addTodo('text12312131'));
     }
+    const MUIclasses = useStyles()
 
     return <div className={classes.content}>
+        <Typography className={MUIclasses.title}>What would you like to learn today?</Typography>
         <div className={classes.buttonsWrapper}>
             {bubbleList.map((bubble) => {
                 return <Bubble key={bubble.id} type={bubble.content}>{bubble.content}</Bubble>
             })}
         </div>
-        <Button className={classes.addBubble} variant="contained" color="primary" onClick={openMenteeModal}>
+        <Button className={MUIclasses.addButton} variant="outlined" color="primary" onClick={openMenteeModal} endIcon={<ButtonArrow/>}>
             <svg className="MuiSvgIcon-root jss82 MuiSvgIcon-fontSizeSmall" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style={{'width': '18px','fill': 'white'}}>
                 <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
             </svg>
-            Action
+            Add Request
         </Button>
         {isMenteeModalOpen && <SimpleModal body={<MenteeInput></MenteeInput>} isOpen={true}></SimpleModal>}
     </div>
--- src/component/content/content-styles.js
+++ src/component/content/content-styles.js
@@ -5,9 +5,8 @@ const contentStyles = makeStyles(() => ({
         display: 'flex',
         justifyContent: 'center',
         flexDirection: 'column',
-        alignItems: 'center',
-        padding: '125px 0 0',
         margin: '0 auto 30px',
+        width:'72%'
     },
     buttonsWrapper: {
         display: 'flex',
@@ -15,14 +14,7 @@ const contentStyles = makeStyles(() => ({
         marginBottom: '50px',
         flexWrap: 'wrap',
     },
-    addBubble:  {
-        background: '#2196F3',
-        color: 'white',
-        width: '126px',
-        height: '40px',
-        boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.2), 0px 3px 4px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.14)',
-        borderRadius: '100px',
-    }
+
 }))
 
 export default contentStyles;
